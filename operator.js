b5a139dfeb06477c16ed54109b3612ede498b34a
Aradhya <soularadhya@gmail.com>
2026-01-20 23:41:37 +0530

    Add operator.js

---
+++ operator.js
@@ -0,0 +1,22 @@
+//javaScript operator
+
+
+//Basic Operator
+let inbox = 10;
+
+console.log(inbox + 10);
+console.log(inbox - 10);
+console.log(inbox * 10);
+console.log(inbox / 10);
+console.log(inbox % 10);
+console.log(inbox ** 10);
+// just replace + with other operators
+
+
+inbox++;;
+//it just means +1
+console.log(inbox);//we will get 11
+
+
+//if we want to add larger number 
+inbox +=20;
