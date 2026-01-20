6f87450ceb7813d8c522feb693ca4ae752249954
Aradhya <soularadhya@gmail.com>
2026-01-20 23:41:51 +0530

    Add concatination.js

---
+++ concatination.js
@@ -0,0 +1,40 @@
+const bank1 = 100;
+const bank2 = 50;
+
+const allBanks = bank1 + bank2;
+console.log(allBanks);
+
+ const greeting = "Welcome to our website ";
+ const user ="rohit";
+
+ console.log(greeting + user);
+
+
+adding quotes in string
+
+const greeting = "Elon musk once said \"life is good\"";
+const user =" rohit";
+
+console.log(greeting + user);
+
+//to solve this we can use single quote at start instead of double quotes 
+//but suppose string has text like "its" so it will create confusion so we use `` backticks 
+
+
+const user = "rohit";
+const greeting = `hey there welcome to our ${user}`;
+
+console.log(greeting);
+
+
+ 
+
+
+
+
+
+
+
+
+
+
