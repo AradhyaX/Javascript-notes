40afec9a03bfd2bb408339f26d375952ecc3ef32
Aradhya <soularadhya@gmail.com>
2026-02-20 23:37:45 +0530

    Add 05_function

---
+++ 05_function/script.js
@@ -0,0 +1,34 @@
+// A javaScript function is a block of code designed to perform a particular task.
+
+// A FUNCTION DECLARATION (defining a function)
+/*
+function square(number) {
+  return number*number;
+}
+
+// A FUNCTION CALL (calling/executing a function)
+const result = square(5) // it store return value value of function square to result variable
+
+console.log(result)
+
+// function [functionName]([placeholder]
+ */
+
+
+
+// A FUNCTION DECLARATION
+
+function name(parameter){
+  //statements
+  // have access to "this" keyword
+}
+
+// A FUNCTION EXPRESSION
+const name = function(parameter){
+  //statements
+}
+
+// An ARROW FUNCTION (Modern way)
+const name =(parameter)=> {
+  //statements
+}
