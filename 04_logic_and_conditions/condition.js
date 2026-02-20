cf42edc1eaf3f62342d90dbe6391546b858255fc
Aradhya <soularadhya@gmail.com>
2026-02-20 23:37:40 +0530

    Add 04_logic_and_conditions

---
+++ 04_logic_and_conditions/condition.js
@@ -0,0 +1,63 @@
+// == they are used for comparison
+// 5 == 5 true
+// 5 == 6 false
+// 5 != 5 false
+// 2 != 3 true
+
+// Not Equal
+// 4 ! = 3 true
+// 4 != 4 false
+
+// 5 > 10 false
+// 10 > 6 true
+// 10 >= 10 true
+// 20 <= 20 true
+
+
+// === USED TO CHECK DATA TYPE OF THE VALUE
+
+// NEVER EVER USE == EQUAL 
+// ALWAYS USE === EQUALS
+
+// 10 ==="10" false
+
+
+//if else Statement
+const access = true;
+
+if(access) {
+     console.log('hello meow meow')
+}else{
+  console.log('access denied')
+}
+
+
+const age = 20;
+
+if(age >=18){
+ console.log('eligible for vote')
+}else{
+  console.log('not eligible for vote' )
+}
+
+// && OPERATOR it checks if both value is true
+// in below example both age and bank value must be true , if any one is false it will run else 
+
+const age = 20
+const bank = 20
+
+if{(age >= 18 && bank > 15)
+  console.log('you can enter')
+}else {
+  console.log('you cant enter')
+}
+
+
+// OR OPERATOR(||) if any 1 value is true among age and bank it will run if
+|| 
+
+
+// NOT OPERATOR ! 
+//
+
+IF (age != 15)
---
+++ 04_logic_and_conditions/switch.js
@@ -0,0 +1,18 @@
+const superhero = 'Thorr';
+
+switch(superhero){
+     case'Iron Man':
+         console.log('I am Iron Man...')
+         break;
+      case'Thor':
+         console.log('That is my hammer!')
+          break;
+      case'Captain America':
+          console.log('never give up')
+          break;
+      case'Black Widow':
+          console.log('one shot, one kill')
+          break;
+      default:
+            console.log('Enter a valid superhero name')
+}
