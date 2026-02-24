bd9632d5bee59ccd7d4d794fdad57d3be7d8402c
Aradhya <soularadhya@gmail.com>
2026-02-24 23:21:34 +0530

    update 05_function/script.js with function notes

--- 05_function/script.js
+++ 05_function/script.js
@@ -1,35 +1,48 @@
-// A javaScript function is a block of code designed to perform a particular task.
+//? A javaScript function is a block of code designed to perform a particular task.
 
-// A FUNCTION DECLARATION (defining a function)
-/*
+//! A FUNCTION DECLARATION (defining a function)
+
 function square(number) {
   return number*number;
 }
 
-// A FUNCTION CALL (calling/executing a function)
+//! A FUNCTION CALL (calling/executing a function)
+
 const result = square(5) // it store return value value of function square to result variable
 
 console.log(result)
 
-// function [functionName]([placeholder]
- */
+// ======================================================
+//? function [functionName]([placeholder]
+// ======================================================
 
 
 
-// A FUNCTION DECLARATION
+//! A FUNCTION DECLARATION
 
 function name(parameter){
   //statements
   // have access to "this" keyword
 }
 
-// A FUNCTION EXPRESSION
+//! A FUNCTION EXPRESSION
 const name = function(parameter){
   //statements
 }
 
-// An ARROW FUNCTION (Modern way)
+//! An ARROW FUNCTION (Modern way)
 const name =(parameter)=> {
   //statements
 }
 
+
+//? a function is executed only when it is called this process is called invocation
+
+function sayHi(name) {
+  console.log(`Hi, ${name}`);
+}
+
+sayHi(`joe`);
+
+
+//? Every function in javaScript returns undefined
