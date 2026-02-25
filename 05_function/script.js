3c58332a86c715e3d1db806da05a57b23c1a8b19
Aradhya <soularadhya@gmail.com>
2026-02-25 22:43:57 +0530

    add notes on JavaScript functions, arrow functions, and default parameters

--- 05_function/script.js
+++ 05_function/script.js
@@ -8,7 +8,7 @@ function square(number) {
 
 //! A FUNCTION CALL (calling/executing a function)
 
-const result = square(5) // it store return value value of function square to result variable
+const result = square(5) // it store return value of function square to result variable
 
 console.log(result)
 
@@ -45,4 +45,54 @@ function sayHi(name) {
 sayHi(`joe`);
 
 
-//? Every function in javaScript returns undefined
+//? Every function in javaScript returns undefined 
+
+function add(a, b){
+  return a + b;
+}
+
+const sum = add(2, 2);
+console.log(sum);
+
+
+//=====================================
+function test(){
+  return true;
+  return false;
+}
+
+const bool = test();
+console.log(bool);
+
+//? if there are multiple return statement only the first one gets executed others get greyed out.
+
+
+//! ARROW FUNCTION
+
+const square = (number) => {
+      return number * number;
+}
+
+      const result = square(8);
+      console.log(result)
+
+
+//! PARAMETERS - used when defining a func
+//! ARGUMENTS - real values passed when making a func call
+
+const sayHi = (name = 'jane', age = 0) => {
+  console.log(`Hi, ${name} is ${age} years old.`);
+}
+
+sayHi('joe',25); 
+//? name = jane or age = 0 are default paramenters if someone does not pass an argument
+
+const add = (a, b = 0) =>{
+  return a + b;
+
+}
+const result = add(2);
+
+console.log(result);
+
+//? without default value if we only pass one argument then we will get NAN so we use = 
