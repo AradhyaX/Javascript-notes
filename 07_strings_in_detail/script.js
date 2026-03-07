de47497e9eeab15b3ee41a5ea9303404c27b406c
Aradhya <soularadhya@gmail.com>
2026-03-07 22:23:17 +0530

    Add strings in detail notes

--- 07_strings_in_detail/script.js
+++ 07_strings_in_detail/script.js
@@ -1,3 +1,12 @@
 const single = 'This is a string';
 const double = 'This is a string';
 const backticks =`This is a string`;
+
+const sum = (a,b) => a+b;
+
+const total = `The sum is ${sum(2,10)}`
+
+console.log(total);
+
+const greeting = `Hi I'am Aradhya, but people call me "adi"`;
+console.log(greeting);
