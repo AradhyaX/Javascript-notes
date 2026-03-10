973c0fcbcbf96d79c53683292dd147ba876e5c0f
Aradhya <soularadhya@gmail.com>
2026-03-10 23:53:07 +0530

    Add uppercase and lowercase string methods

--- 08_strings_in_detail/script.js
+++ 08_strings_in_detail/script.js
@@ -1,7 +1,18 @@
-const name = 'Johm';
-// console.log(name.length);
+// const name = 'Johm';
+// // console.log(name.length);
 
-const firstLetter = name[0];
-const lastLetter = name[3];
+// const firstLetter = name[0];
+// const lastLetter = name[3];
 
-console.log(firstLetter, lastLetter);
+// console.log(firstLetter, lastLetter);
+
+
+//! UPPERCASE and lowercase letters
+
+const mixedCaseString = 'Hello! How are you?';
+
+const lowerCaseString = mixedCaseString.toLowerCase();
+const upperCaseString = mixedCaseString.toUpperCase();
+
+
+console.log(upperCaseString);
