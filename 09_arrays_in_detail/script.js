0f8ecc944763f6fcc89d90b9cd738c16c75b91f9
Aradhya <soularadhya@gmail.com>
2026-03-15 23:05:51 +0530

    Add arrays in detail notes

---
+++ 09_arrays_in_detail/script.js
@@ -0,0 +1,14 @@
+const months = ['January', 'Februrary', 'March', 'April'];
+
+months[2] = 'Not March';
+
+console.log(months);
+console.log(months.length);
+
+// for loop
+const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday'];
+for (let i = 0; i < days.length; i++) {
+  console.log(days[i]);
+  
+}
+
