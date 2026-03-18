79b9863368e0df345df94a35ff56276318703d36
Aradhya <soularadhya@gmail.com>
2026-03-18 23:54:43 +0530

    Add array methods notes (forEach, Slice, Splice)

--- 09_arrays_in_detail/script.js
+++ 09_arrays_in_detail/script.js
@@ -12,25 +12,48 @@ for (let i = 0; i < days.length; i++) {
   
 }
 
+//! Array Methods
 
 const name = ['jon','Bob','David','Mark'];
 
-//! Array Push - Adds a new element containing the entered value to the end of the array.
-
+//? Array Push - Adds a new element containing the entered value to the end of the array.
 names.push('Dean');
 
-console.log(length);
+console.log(names);
 
-//! Array Pop - Deletes the last element of an array
+//? Array Pop - Deletes the last element of an array
 names.pop();
 
-//! Array Shift - Deletes the first element of an array
+//* we dont need to add value it will just delete the last element from array.
+
+//? Array Shift - Deletes the first element of an array
 names.shift();
 
-//! Array Unshift - Adds a new value to the start of an array
+//? Array Unshift - Adds a new value to the start of an array
 names.unshift('Dean');
 
-//! Array Splice
-names.Splice(2,2,'Jenny','Johnny');
+//? Array Splice - it adds/remove value in any position of an array
+names.Splice(2,0,'Jenny','Johnny');
 
 console.log(names);
+
+//? Array Slice - Copies certain parts of an array into newly created array
+const noOneLikesJon = names.slice(1);
+
+console.log(noOneLikesJon);
+
+
+//! Array forEach -  it performs an action for each element in an array.
+
+const names = ['Jon','Jenny','Johnny'];
+
+for (let i = 0; i<names.length; i++) {
+  console.log(i, names[i]);
+}
+
+names.forEach((name, i) =>{
+console.log(name, i);
+
+});
+
+
